6652b320592b81c601211b341a78b914df3503be
Omer Ben Yaakov <omerbenyaakov@gmail.com>
2022-07-13 17:37:06 +0300

    Initial Commit.

---
+++ Models/UserDto.cs
@@ -0,0 +1,13 @@
+
+namespace YoutCubeEntitiesMicroservice.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Salt { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
